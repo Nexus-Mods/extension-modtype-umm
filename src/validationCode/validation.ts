2987153b191cc138123399036567c043c54098d8
TanninOne <seppleviathan@gmx.de>
2021-10-08 09:16:49 +0200

    removed generated code

--- src/validationCode/validation.ts
+++
@@ -1,4 +0,0 @@
-import validateQpuWH from "./types.validate";export function validateIUMMGameConfig(data): any[] {
-    var res = validateQpuWH(data);
-    return (res === false) ? validateQpuWH.prototype.constructor.errors : [];
-}
